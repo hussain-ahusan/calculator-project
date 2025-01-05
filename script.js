466f97b8098aa5e4bcf4269266ca75d12ec1f7ce
hussian-ahusan <93662141+hussain-ahusan@users.noreply.github.com>
2025-01-05 15:30:01 +0500

    Basic functions complete.

---
+++ script.js
@@ -0,0 +1,23 @@
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const multipily = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
+
+
+function operate (operator, a, b) {
+    switch (operator){
+        case '+':
+            return add (a, b)
+        case '-':
+            return subtract (a, b)
+        case '*':
+            return multipily (a, b)
+        case '/':
+            return divide (a, b)
+        default:
+            return "";
+    };
+};
