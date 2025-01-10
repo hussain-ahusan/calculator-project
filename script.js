cfed152955745a9d6bbd40206cd5c0e03b11ddd3
hussian-ahusan <93662141+hussain-ahusan@users.noreply.github.com>
2025-01-11 00:41:15 +0500

    Calculations can now be performed

--- script.js
+++ script.js
@@ -1,30 +1,72 @@
-const add = (a, b) => a + b;
-
-const subtract = (a, b) => a - b;
-
-const multipily = (a, b) => a * b;
-
-const divide = (a, b) => a / b;
+const clear = document.querySelector('.AC');
+clear.addEventListener('click', () => {
+    const input = document.querySelector('input');
+    input.value = "";
+});
 
 
-function operate (operator, a, b) {
-    switch (operator){
-        case '+':
-            return add (a, b)
-        case '-':
-            return subtract (a, b)
-        case '*':
-            return multipily (a, b)
-        case '/':
-            return divide (a, b)
-        default:
-            return "";
+function updateDisplay () {
+    const input = document.querySelector('input');
+    const buttons = document.querySelectorAll(".number, .opr");
+    buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        input.value += e.target.textContent;
+    })
+})
+};
+
+function updateDisplay(){
+    const operators = document.querySelectorAll('.opr')
+    const numbers = document.querySelectorAll('.number')
+    numbers.forEach((number) => {
+        const input = document.querySelector('input');
+        number.addEventListener('click', (e) => {
+            input.value += e.target.textContent;
+        })
+    })
+    
+    operators.forEach((operator) => {
+        operator.addEventListener('click', (e) =>{
+            const input = document.querySelector('input');
+            if (e.target.textContent == '=') return;
+            input.value += e.target.textContent;
+        })
+    })
+}
+
+function operate (operator, num1, num2) {
+    const input = document.querySelector('input').value;
+    num1 = parseInt(input.split(/[*]|[-]|[+]|[\/]/)
+    .splice(0, 1)
+    .join());
+    console.log(num1);
+    num2 = parseInt(input.split(/[*]|[-]|[+]|[\/]/)
+    .splice(-1)
+    .join());
+    console.log(num2);
+    operator = input.split(/[^-|+|*|\/]/)
+    .filter(e => e)
+    .join();
+    console.log(operator)
+    if (operator == '+') {
+        return num1 + num2
+    } else if (operator == '-') {
+        return num1 - num2
+    } else if (operator == '*') {
+        return num1 * num2
+    } else if (operator == '/') {
+        return num1 / num2
+    } else if (operator == '%') {
+        return (num1- num2) / num1 * 100
+    } else {
+        return 0;
     };
 };
-const input = document.querySelector('input');
-const numbers = document.querySelectorAll(".number");
-numbers.forEach((number) => {
-    number.addEventListener('click', (e) => {
-        input.value = e.target.textContent;
-    })
-});
+
+
+const equal = document.querySelector('.equal');
+equal.addEventListener('click', () => {
+    const input = document.querySelector('input');
+    input.value = operate()
+})
+updateDisplay()
